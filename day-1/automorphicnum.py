075037edd4c67e62cc6b1962cd28c589eefc67de
shruthi <shruti@gmail.com>
2026-03-12 11:33:15 +0530

    first commit

--- day-1/automorphicnum.py
+++ day-1/automorphicnum.py
@@ -1,13 +1,10 @@
 num = int(input("Enter a number: "))
 square = num * num
-
 temp = num
 count = 0
-
 while temp > 0:
     count = count + 1
     temp = temp // 10
-
 if square % (10 ** count) == num:
     print("Automorphic Number")
 else:
---
+++ day-1/discount.py
@@ -0,0 +1,6 @@
+n1=float(input("Enter the original price: "))
+n2=float(input("Enter the discount percentage: "))
+discount_amount=(n1*n2)/100
+final_price=n1-discount_amount
+print("The discount amount is: ",discount_amount)
+print("The final price after discount is: ",final_price)
